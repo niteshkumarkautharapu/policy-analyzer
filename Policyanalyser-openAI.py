737843842916b43d1a9ff271bf0b0fd8b2978cbf
niteshkumarkautharapu <niteshkumar.230@gmail.com>
2026-04-06 21:45:54 +0530

    Update Policyanalyser-openAI.py

--- Policyanalyser-openAI.py
+++ Policyanalyser-openAI.py
@@ -10,22 +10,6 @@ from datetime import datetime
 from notion_client import Client
 
 
-# Google Analytics
-GA_ID = "G-0D8JEGK5JK"
-
-st.markdown(
-    f"""
-    <script async src="https://www.googletagmanager.com/gtag/js?id={GA_ID}"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){{dataLayer.push(arguments);}}
-      gtag('js', new Date());
-      gtag('config', '{GA_ID}');
-    </script>
-    """,
-    unsafe_allow_html=True
-)
-
 def save_feedback(policy, report, feedback, comment):
 
     try:
